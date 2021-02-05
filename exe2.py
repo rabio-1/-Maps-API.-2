b964e347bce1d76569b74d25af8f092cae773e5b
rabio-1 <74779779+rabio-1@users.noreply.github.com>
2021-02-05 17:36:16 +0300

    Add files via upload

---
+++ exe2.py
@@ -0,0 +1,55 @@
+import os
+import sys
+
+import pygame
+
+from Samples.geocoder import get_coordinates, get_ll_span
+from Samples.mapapi_PG import show_map
+
+
+if __name__ == '__main__':
+    toponym_to_find = ' '.join(sys.argv[1:])
+    if toponym_to_find:
+        zoom = 15
+        lat, lon = get_coordinates(toponym_to_find)
+        ll_spn = f'll={lat},{lon}&z={zoom}'
+        point_param = f'pt={lat},{lon}'
+        show_map(ll_spn, 'map')
+    else:
+        print('Нет данных для поиска')
+
+    map_file = 'map.png'
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                if event.key == pygame.K_PAGEUP:
+                    if zoom < 23:
+                        zoom += 1
+                if event.key == pygame.K_PAGEDOWN:
+                    if zoom > 1:
+                        zoom -= 1
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 4:
+                    if zoom < 23:
+                        zoom += 1
+                if event.button == 5:
+                    if zoom > 1:
+                        zoom -= 1
+
+        ll_spn = f'll={lat},{lon}&z={zoom}'
+        point_param = f'pt={lat},{lon}'
+        show_map(ll_spn, 'map', point_param)
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        # Переключаем экран и ждем закрытия окна.
+        pygame.display.flip()
+
+    pygame.quit()
+    os.remove(map_file)
